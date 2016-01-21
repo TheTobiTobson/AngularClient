35fb198f19ee4ac8132236d5f125202565cbb2e4
TobiTob <tobi.wurm@gmail.com>
2016-01-21 11:34:54 +0100

    Initialer Commit
    Der Route-Mechanismus wurde implementiert

---
+++ mainapp.js
@@ -0,0 +1,32 @@
+﻿'use strict';
+
+// module declaration
+angular.module('startpageModule', []);
+
+angular.module('feedMeMainApp', [
+    'ngRoute',
+    'startpageModule'
+])
+
+
+.config(['$routeProvider', function ($routeProvider) {
+
+    $routeProvider
+        .when('/startpage', {
+            controller: 'startpageController',
+            templateUrl: 'routes/startpage/startpage.html'
+        })
+
+        //.when('/account', {
+        //    controller: 'AccountController',
+        //    templateUrl: 'modules/account/views/account.html'
+        //})
+
+
+        //.when('/', {
+        //    controller: 'HomeController',
+        //    templateUrl: 'modules/home/views/home.html'
+        //})
+
+        .otherwise({ redirectTo: '/startpage' });
+}])
---
+++ routes/startpage/startpageController.js
@@ -0,0 +1,12 @@
+﻿'use strict';
+
+//Reopen Module
+angular.module('startpageModule')
+
+.controller('startpageController',
+    ['$scope',
+        function ($scope) {
+            //$scope.$log.log('Modul: Authentication - .controller - Entered');
+            alert('hi');
+    }]);
+
