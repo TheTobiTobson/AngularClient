f35ef37872cfce34df2626e8896c22a7f2542cc9
TobiTob <tobi.wurm@gmail.com>
2016-02-08 00:13:19 +0100

    Sicherung vor Umzug der Funktion Dynamische Fragen in designer.html

--- routes/about/aboutController.js
+++ routes/about/aboutController.js
@@ -15,7 +15,47 @@ angular.module('aboutModule')
                 { "Titel": "Vier", "Type": "JaNein", "Text": "" }
             ];
 
-            $scope.anzeige = "hihih";
+            // Update(PUT) one Question
+            $scope.updateQuestion = function (QUE_id_of_question_to_update) {
+                alert(QUE_id_of_question_to_update);
+            };
+
+            //DropDow
+            // QuestionTypesOptions
+            $scope.questionTypes = [               
+                 "Freitext",
+                 "Auswahl"
+            ];
+
+            //DropDow
+            // Show or hide question
+            $scope.dropdownShowHide = [
+                 "Show",
+                 "Hide"
+            ];
+
+
+            // Testdaten
+            $scope.Questions = [{
+                "QUE_id": 22,
+                "QUE_FBS_id": 1,
+                "QUE_type": "Freitext",
+                "QUE_position": 5,
+                "QUE_text": "Hallo alle zusammen",
+                "QUE_answerRadioButton": "",
+                "QUE_title": "KING",
+                "QUE_showQuestionInFeedback": true
+            },
+            {
+                "QUE_id": 33,
+                "QUE_FBS_id": 3,
+                "QUE_type": "Auswahl",
+                "QUE_position": 2,
+                "QUE_text": "hihihih",
+                "QUE_answerRadioButton": "",
+                "QUE_title": "QUEEN",
+                "QUE_showQuestionInFeedback": false
+            }];
 
 }]);
 
