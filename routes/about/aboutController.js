82a2059a39cc0037ad3e6cdd723af3ba636d3f2a
TobiTob <tobi.wurm@gmail.com>
2016-02-20 17:27:09 +0100

    LESS files, Sidenav und flexboxen in about-page eingebaut

--- routes/about/aboutController.js
+++ routes/about/aboutController.js
@@ -4,12 +4,24 @@
 angular.module('aboutModule')
 
 .controller('aboutController',
-    ['$scope',
-        function ($scope) {
+    ['$scope','$mdSidenav','$mdMedia',
+        function ($scope, $mdSidenav, $mdMedia) {
             $scope.$log.log('aboutModule          - aboutController      - .controller          - Entered');
 
-           
+            // Header Variables
+            $scope.about = {};
+            $scope.about.pagename = "About";
 
-            
+            //Open NavBar
+            $scope.openLeftMenu = function () {
+                $mdSidenav('leftSideNavBar').toggle();
+            };
+
+            $scope.$watch(function () { return $mdMedia('lg'); }, function (big) {
+                $scope.AAAbigScreen = big;
+            });
+            $scope.AAAscreenIsSmall = $mdMedia('sm');
+            $scope.AAAcustomQuery = $mdMedia('(min-width: 1234px)');
+            $scope.AAAanotherCustom = $mdMedia('max-width: 300px');
 }]);
 
